0ae62cd92f361d99392cc9494ba68c52c9e6087d
Barbara Scatolin <scatolinbarbara@gmail.com>
2022-08-03 10:05:09 -0300

    primeira atualização no JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,34 @@
-const cipher = {
-  // ...
-};
+const cipher = {encode, decode};
 
-export default cipher;
+function encode(offset, text) {
+  
+  let mensagem = " ";
+  for (let i = 0; i < texto.length; i++) {
 
-function offset(texto) {}
+  const numberASCII = text.charCodeAt(i);
+
+  if (numberASCII >= 65 && numberASCII <= 90) {
+    const formula = (((numberASCII - 65) + offset) %26) + 65;
+    mensagem +- String.fromCharCode(formula);
+  } else if (numberASCII >= 97 || numberASCII <= 122) {
+    mensagem +- String.fromCharCode(numberASCII);
+  }
+  } return mensagem;
+}
+
+function decode(offset,text) {
+
+  let mensagem = " ";
+  for (let i = 0; i < text.length; i++) {
+
+    const numberASCII = text.charCodeAt(i);
+
+    if (numberASCII >= 65 && numberASCII <= 90) {
+      const formula = (((numberASCII + 65) = offset) % 26) + 65; 
+      mensagem +- String.fromCharCode(formula);
+  } else if (numberASCII >= 97 && numberASCII <= 122) {
+    const formula = (((numberASCII + 97) - offset) % 26) + 97;
+    mensagem +- String.charCodeAt(formula)
+  }
+} return mensagem;
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const txtEncode = document.getElementById("textoCodificado")
+const txtDecode = document.getElementById("textoDescodificado")
+
+txtEncode.addEventListener("keyup" , (  ) => {
+  let text = document.getElementById("txtCodificafado").value;
+  let offset = perseInt(document.getElementById("inpkey").value);
+  let code = cipher.encode(offset,text);
+  const res = document.getElementById("textoDescodificafado");
+  res.innerHTML = code;
+});
+
+txtDecode.addEventListener("keyup" , ( ) => {
+  let text = document.getElementById("textoDescodificafado").value;
+  let offset = perseInt(document.getElementById("inpkey").value);
+  let code = cipher.decode(offset,text);
+  const res = document.getElementById("textoCodificafado")
+});
+
+
+/*
+botaoDecifrar.addEventListener("click", function encode(texto, offset){
+  const texto= mensagem
+})*/
+
+documento.getElementById("botaoCifrar").addEventListener("click",validarMensagem )
+
+function validarMensagem() {
+  if (document.getElementById("botaoCifrar").value != " "&&
+  document.getElementById("botaDecifrar").value !=" " &&
+  document.getElementById("texto").value !=" "){
+    alert("Esta quase lá!")
+  }else{
+    alert("Por favor, preencha os campos offset e a mensagem");
+  }
